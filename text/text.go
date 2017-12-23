3209151febe6e365735f5a226d7d64901bf2da76
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2017-12-23 07:02:16 -0600

    reorg

--- text/text.go
+++ text/text.go
@@ -45,6 +45,25 @@ func Render(text string, x, y float32) {
 
 	gl.VertexAttribPointer(attr_pos, 4, gl.FLOAT, false, 0, nil)
 	for _, chr := range text {
-		println(chr)
+		var g truetype.GlyphBuf
+		Check(g.Load(font, 12, font.Index(chr), 0))
+
+		max := g.Bounds.Max
+		w := float32(max.X)
+		h := float32(max.Y)
+		gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RED, int32(w), int32(h), 0, gl.RED, gl.UNSIGNED_BYTE, gl.Ptr(g.Points))
+		min := g.Bounds.Min
+		x2 := x + float32(min.X)*12
+		y2 := -y + float32(min.Y)*12
+		w *= 12
+		h *= 12
+		box := []float32{
+			x2, -y2, 0, 0,
+			x2 + w, -y2, 1, 0,
+			x2, -y2 - h, 0, 1,
+			x2 + w, -y2 - h, 1, 1,
+		}
+		gl.BufferData(gl.ARRAY_BUFFER, 4*len(box), gl.Ptr(box), gl.DYNAMIC_DRAW)
+		gl.DrawArrays(gl.TRIANGLE_STRIP, 0, 4)
 	}
 }
